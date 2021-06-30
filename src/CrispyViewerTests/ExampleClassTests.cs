d0f9d39ed7be593c95747415c0bd52cc1e2a6432
h4570 <sandro.sobczynski@gmail.com>
2021-07-01 00:58:38 +0200

    feat(Tests): added tests project

---
+++ src/CrispyViewerTests/ExampleClassTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using CrispyViewer;
+
+namespace CrispyViewer.Tests
+{
+    public class ExampleClassTests
+    {
+        [Fact()]
+        public void Get1Test()
+        {
+            var exClass = new ExampleClass();
+            Assert.Equal(1, exClass.Get1());
+        }
+    }
+}
