eb096f76dab93611ffd614112311a4cc7380810a
szymon.sliwinski@interia.pl <szymon.sliwinski@interia.pl>
2021-07-12 19:09:02 +0200

    feat(BMPLoader): getting bmp resolution from it's bytes

--- src/FileLoader/ImageLoaders/BMPLoader.cs
+++ src/FileLoader/ImageLoaders/BMPLoader.cs
@@ -1,13 +1,47 @@
 ﻿using FileLoader.Interfaces;
+using System.IO;
 using System;
+using Shared.Structs;
+using FileLoader.structs;
 
 namespace FileLoader.ImageLoaders
 {
     public class BMPLoader : IImageLoader
     {
         public IImage Load(string path)
-        {
-            throw new NotImplementedException();
+        {            
+            try
+            {
+                var bmpBytes = File.ReadAllBytes(path);
+                var bmpFile = new BMPFile();
+
+                var resolutions = GetBMPResolutions(bmpBytes);
+                bmpFile.Width = resolutions.Item1;
+                bmpFile.Height = resolutions.Item2;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+            return null;
         }
+
+        public (int, int) GetBMPResolutions(byte[] BMPBytes)
+        {
+            var widthBytes = new byte[4];
+            var heightBytes = new byte[4];
+            Array.Copy(BMPBytes,18,widthBytes,0, 4);
+            Array.Copy(BMPBytes,22, heightBytes,0, 4);
+
+            return (BitConverter.ToInt32(widthBytes, 0),
+                BitConverter.ToInt32(heightBytes, 0));
+                       
+        }
+
+        public RGB[] GetBMPColors(byte[] BMPBytes)
+        {
+            return null;
+        }
+
     }
 }
---
+++ src/FileLoader/structs/BMPFile.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Structs;
+
+namespace FileLoader.structs
+{
+    public struct BMPFile
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public RGB PixelColors { get; set; }
+    }
+}
